478bf86d7b1db6ec74b0f1a3dc48338213a9bd19
aaron-edmiston <acedmiston@yahoo.com>
2021-06-07 22:03:23 +0100

    added some css

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Input from './components/Input';
+
+class App extends Component {
+  state = {
+    originCountry: { Places: [] },
+    destinationCountry: { Places: [] }
+  }
+  render() {
+    console.log(this.state.originCountry);
+    return (
+      <div className="outer-box">
+        <div className="input-field">
+          <Input type={'originCountry'} Places={this.state.originCountry.Places} onInput={this.onInput} />
+          <Input type={'destinationCountry'} Places={this.state.destinationCountry.Places} onInput={this.onInput} />
+          <input type="date"></input>
+          <input type="date"></input>
+          <input type='number'></input>
+        </div>
+      </div>
+    );
+  }
+
+  onInput = (e) => {
+    const options = {
+      method: 'GET',
+      url:
+        'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/',
+      params: { query: e.target.value },
+      headers: {
+        'x-rapidapi-key': '3737c740damsh294914373cea252p10fc24jsnd39fd87ca55c',
+        'x-rapidapi-host':
+          'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+      },
+    };
+    this.getAPIData(e.target.id, options);
+  }
+
+  async getAPIData(id, options) {
+    const result = await axios.request(options);
+    this.setState({ [id]: result.data });
+    console.log(this.state);
+  }
+}
+
+
+export default App;
